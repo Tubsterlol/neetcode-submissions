721fe2ba00020029f39f352aba37ed78aa25f5cd
Asit Patel <asitpatel2005@gmail.com>
2026-09-20 22:08:17 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        mapS, mapT = {}, {}
+
+        if len(s) != len(t):
+            return False
+        
+        for i in range(len(s)):
+            if s[i] not in mapS:
+                mapS[s[i]] = 1
+            else:
+                mapS[s[i]] += 1
+
+        for j in range(len(t)):
+            if t[j] not in mapT:
+                mapT[t[j]] = 1
+            else:
+                mapT[t[j]] += 1
+
+        if mapS == mapT:
+            return True
+        else:
+            return False
